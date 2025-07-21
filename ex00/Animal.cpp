4024378165e36a90dfdbe63c457b08e4ba40af7c
pavel <A1XP>
2025-07-21 17:16:56 +0200

    first commit

---
+++ ex00/Animal.cpp
@@ -0,0 +1,36 @@
+#include "Animal.hpp"
+#include <string>
+#include <iostream>
+
+Animal::Animal() : _type("Animal")
+{
+	std::cout << _type << " has been created!" << std::endl;
+}
+
+Animal::Animal(const Animal& other) : _type(other._type)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& other)
+{
+	std::cout << "Animal copy assignment operator called" << std::endl;
+	if (this != &other)
+		_type = other._type;
+	return (*this);
+}
+
+Animal::~Animal()
+{
+	std::cout << _type << " is destroyed." << std::endl;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << _type << " trtrtrtr!" << std::endl;
+}
+
+const std::string& Animal::getType() const
+{
+	return (this->_type);
+}
