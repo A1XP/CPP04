8693411077a305f129ef2428dad196ebeeceef54
pavel <A1XP>
2025-07-28 19:27:16 +0200

    main test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -15,7 +15,7 @@ int main()
     AMateria* tmpCure = new Cure();
     src->learnMateria(tmpCure);
     delete tmpCure;
-    
+
     ICharacter* me = new Character("me");
 
     AMateria* tmp;
@@ -28,6 +28,11 @@ int main()
     me->use(0, *bob);
     me->use(1, *bob);
 
+    me->unequip(0);
+    me->unequip(1);
+    me->unequip(2);
+    me->unequip(3);
+    
     delete bob;
     delete me;
     delete src;
